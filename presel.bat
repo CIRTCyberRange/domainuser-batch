ef2776aea113965015c8ef1716fbd42a77b76673
palash009 <52623155+palash009@users.noreply.github.com>
2023-08-30 11:04:46 +0600

    Add files via upload

---
+++ presel.bat
@@ -0,0 +1,7 @@
+net user abeyta Abdc@2023 /add
+net user abfarad Abdc@2024 /add
+net user abhenry Abdc@2025 /add
+net user abhor Abdc@2026 /add
+net user abhorrence Abdc@2027 /add
+net user abhorrent Abdc@2028 /add
+net user abib Abdc@2029 /add
